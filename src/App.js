a3f7832b33997e599c2573e0c995ec96a6720896
ANISH0309 <mittalanish789@gmail.com>
2021-07-04 17:26:44 +0530

    Initial project setup
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-300 font-bold mt-12">
+        React and tailwind CSS working
+      </h1>
+    </div>
+  );
+}
+
+export default App;
