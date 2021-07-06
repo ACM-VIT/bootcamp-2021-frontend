446d997a096747b8b10d87f0a586b8a4df1f083c
Likhit Ajeesh <likhitajeesh20@gmail.com>
2021-07-06 23:06:52 +0400

    feat: About page added

--- src/containers/About/about.js
+++ src/containers/About/about.js
@@ -2,30 +2,32 @@ import React from "react";
 
 const About = () => (
   <div>
-    <h1 className="flex justify-center font-poppins font-bold mt-28 text-5xl text-gray-800 text-opacity-90 tracking-tight">
+    <h1 className="flex justify-center font-poppins font-bold mt-32 text-5xl text-gray-900 text-opacity-90 tracking-tight">
       About Bootcamp
     </h1>
 
-    <p className="flex justify-center font-poppins font-medium mx-32 mt-16 text-xl text-black">
+    <p className="flex justify-center font-poppins font-semibold mx-32 mt-20 text-xl text-black">
       ACM Summer Bootcamp is an all-immersive Bootcamp for novices and pros
       alike. We aim to provide you with a hands-on learning experience through
       our accomplished student mentors. Through project-based learning, we will
       help you get started in 4 exciting domains:
     </p>
 
-    <h2 className="flex justify-center font-poppins font-semibold mt-8 text-3xl text-yellow-500 text-opacity-100">
+    <h2 className="flex justify-center font-poppins font-bold mt-12 text-3xl text-yellow-500 text-opacity-100">
       Web Development, App Development, Machine Learning and Design
     </h2>
 
-    <p className="flex justify-center font-poppins font-medium mt-8 text-xl text-black">
+    <p className="flex justify-center font-poppins font-semibold mt-12 text-xl text-black">
       Within two weeks, you will have gained diverse technical knowledge and
       also have incredible projects under your belt.
     </p>
 
-    <p className="flex justify-center font-poppins font-medium mx-40 mt-8 text-xl text-black">
-      Not to forget, we will provide certificates upon successful completion of
-      the Bootcamp and give personal shout-outs to the top projects on our
-      social media handles!
+    <p className="justify-center font-poppins font-semibold mx-40 mt-8 text-xl text-black">
+      Not to forget, we will provide
+      <span className="text-yellow-500"> certificates</span> upon successful
+      completion of the Bootcamp and give personal
+      <span className="text-yellow-500"> shout-outs</span> to the top projects
+      on our social media handles!
     </p>
   </div>
 );
