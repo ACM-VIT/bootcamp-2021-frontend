1a235ea50921453cf953d78b0b31140c2bd84ab2
ANISH0309 <mittalanish789@gmail.com>
2021-07-07 22:50:46 +0530

    feat: add footer
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./containers/Landing/Landing";
 import Accordion from "./components/Faqs/Accordion";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Landing />
       <Accordion />
+      <Footer />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-    },
-    extend: {
-      width: {
-        192: "48rem",
+    screens: {
+      xs: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+
+      extend: {
+        width: {
+          192: "48rem",
+        },
+
+        container: {
+          center: true,
+          padding: "2rem",
+        },
       },
     },
   },
