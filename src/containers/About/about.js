f3cfae30af6f480d49e4a361338889cc22739581
Likhit Ajeesh <likhitajeesh20@gmail.com>
2021-07-09 02:50:51 +0400

    Margin converted to padding in About section

--- src/containers/About/about.js
+++ src/containers/About/about.js
@@ -3,11 +3,11 @@ import React from "react";
 /* Responsive About Page */
 const About = () => (
   <div>
-    <h1 className="flex text-left ml-10 sm:justify-center xs:mr-10 font-poppins font-bold mt-20 sm:mt-32 text-3xl sm:text-4xl md:text-5xl text-gray-900 text-opacity-90 tracking-tight">
+    <h1 className="flex text-left pl-6 sm:justify-center xs:mr-10 font-poppins font-bold mt-20 sm:mt-32 text-3xl sm:text-4xl md:text-5xl text-gray-900 text-opacity-90 tracking-tight">
       About Bootcamp
     </h1>
 
-    <p className="text-left sm:justify-center sm:text-center font-poppins font-semibold mx-10 sm:mx-16 lg:mx-32 mt-10 sm:mt-12 lg:mt-20 text-base sm:text-lg md:text-xl text-black">
+    <p className="text-left sm:justify-center sm:text-center font-poppins font-semibold px-6 sm:mx-16 lg:mx-32 mt-10 sm:mt-12 lg:mt-20 text-base sm:text-lg md:text-xl text-black">
       ACM Summer Bootcamp is an all-immersive Bootcamp for novices and pros
       alike. We aim to provide you with a hands-on learning experience through
       our accomplished student mentors. Through
@@ -15,16 +15,16 @@ const About = () => (
       help you get started in 4 exciting domains:
     </p>
 
-    <h2 className="text-left sm:justify-center sm:text-center font-poppins font-semibold mt-8 sm:mt-12 mx-10 text-lg sm:text-2xl md:text-3xl text-yellow-500 text-opacity-100">
+    <h2 className="text-left sm:justify-center sm:text-center font-poppins font-semibold mt-8 sm:mt-12 px-6 text-lg sm:text-2xl md:text-3xl text-yellow-500 text-opacity-100">
       Web Development, App Development, Machine Learning and Design
     </h2>
 
-    <p className="flex text-left sm:justify-center sm:text-center font-poppins font-semibold mx-10 sm:mx-16 lg:mx-32 mt-8 sm:mt-12 text-base sm:text-lg md:text-xl text-black">
+    <p className="flex text-left sm:justify-center sm:text-center font-poppins font-semibold px-6 sm:mx-16 lg:mx-32 mt-8 sm:mt-12 text-base sm:text-lg md:text-xl text-black">
       Within two weeks, you will have gained diverse technical knowledge and
       also have incredible projects under your belt.
     </p>
 
-    <p className="text-left sm:justify-center sm:text-center font-poppins font-semibold mx-10 sm:mx-16 lg:mx-40 mt-8 mb-10 text-base sm:text-lg md:text-xl text-black">
+    <p className="text-left sm:justify-center sm:text-center font-poppins font-semibold px-6 sm:mx-16 lg:mx-40 mt-8 mb-10 text-base sm:text-lg md:text-xl text-black">
       Not to forget, we will provide
       <span className="text-yellow-500"> certificates</span> upon successful
       completion of the Bootcamp and give personal
--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -18,11 +18,12 @@ const Landing = () => {
   return (
     <>
       <section className="flex pl-6 pr-6 md:pr-0 sm:pl-8 md:pl-12 lg:pl-16 xl:pl-24 md:w-120 lg:w-192 justify-start">
-        <div className="mt-36 md:mt-32 grid z-100">
+        <div className="mt-36 md:mt-20 grid z-100">
           <div>
             <h3 className="font-600 text-custom">ACM-VIT Initiative</h3>
-            <p className="font-600 mt-2 text-landing-xsmall leading-16 sm:text-landing-large md:w-120 lg:w-192 text-custom md:text-6xl xl:text-landing-large lg:text-6xl">
-              Unlock your potential this summer!
+            <p className="font-600 mt-2 text-landing-xsmall md:leading-normal lg:leading-normal xl:leading-normal sm:text-landing-large md:w-120 lg:w-192 text-custom md:text-6xl xl:text-landing-large lg:text-6xl">
+              Unlock your potential this{" "}
+              <span className="text-yellow-400">summer!</span>
             </p>
           </div>
           <div className="flex flex-col md:flex-row mt-12">
@@ -34,9 +35,9 @@ const Landing = () => {
             </span>
           </div>
           <div className="mt-12 flex flex-col md:flex-row">
-            <div className="w-72 h-12 bg-yellow-grad rounded-xl">
+            <div className="w-full xs:w-72 h-12 bg-yellow-grad rounded-xl">
               <input
-                className="font-500 appearance-none rounded-xl border-2 border-yellow-200 text-md w-72 h-12 py-2 px-6 mr-2 leading-tight placeholder-yellow-400 focus:outline-none focus:shadow-outline"
+                className="font-500 appearance-none rounded-xl border-2 border-yellow-200 text-md w-full sm:w-72 h-12 py-2 px-6 mr-2 leading-tight placeholder-yellow-400 focus:outline-none focus:shadow-outline"
                 id="email"
                 type="email"
                 placeholder="Enter your email"
@@ -80,7 +81,7 @@ const Landing = () => {
       </section>
 
       {/* Landing Bottom */}
-      <section className="invisible md:visible px-24 grid grid-cols-2 mt-16">
+      <section className="flex justify-between items-center invisible md:visible px-16 xl:px-24 grid grid-cols-2 mt-16">
         <div className="flex">
           <a
             target="_blank"
